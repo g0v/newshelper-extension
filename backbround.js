bafc2dd69773f2fa4af08b51e0d59ce2329ecf81
Ronny Wang <ronnywang@gmail.com>
2013-08-10 16:38:01 +0800

    加上 backbround.js

---
+++ backbround.js
@@ -0,0 +1,22 @@
+function onRequest(request, sender, sendResponse) {
+    if (request.method== 'page') {
+        // 顯示設定新聞小幫手的 page action
+        chrome.pageAction.show(sender.tab.id);
+    }
+
+    if (request.method == 'add_notification') {
+        var notification = window.webkitNotifications.createNotification(
+              request.image,
+              request.title,
+              request.body
+        );
+        notification.show();
+    }
+
+    // Return nothing to let the connection be cleaned up.
+    sendResponse({});
+};
+
+// Listen for the content script to send a message to the background page.
+chrome.extension.onRequest.addListener(onRequest);
+
