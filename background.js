823a704e57fc2f3ee8030c9724f2a23cc520952c
Ronny Wang <ronnywang@gmail.com>
2013-09-03 18:20:12 +0800

    move db relative code to background and call sendRequest in front

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+var seen_link = {};
+var last_sync_at = -1;
+
 function onRequest(request, sender, sendResponse) {
     if (request.method == 'page') {
         // 顯示設定新聞小幫手的 page action
@@ -8,6 +11,29 @@ function onRequest(request, sender, sendResponse) {
 	add_notification(request.title, request.body, request.link);
     }
 
+    if (request.method == 'start_sync_db') {
+	if (last_sync_at < 0) {
+	    last_sync_at = 0;
+	    setInterval(function(){
+		if ((new Date()).getTime() - last_sync_at < 300 * 1000) {
+		    return;
+		}
+		last_sync_at = (new Date()).getTime();
+
+		sync_db(request.group, sendResponse);
+	    }, 10000);
+	}
+    }
+
+    if (request.method == 'log_browsed_link') {
+	log_browsed_link(request.link, request.title);
+    }
+
+    if (request.method == 'check_report') {
+	check_report(request.title, request.url, sendResponse);
+	return;
+    }
+
     // Return nothing to let the connection be cleaned up.
     sendResponse({});
 };
@@ -15,6 +41,179 @@ function onRequest(request, sender, sendResponse) {
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
 
+
+// sync db from api server
+var sync_db = function(){
+  get_newshelper_db(function(opened_db){
+    get_recent_report(function(report){
+      $.get('http://newshelper.g0v.tw/index/data?time=' + (report ? parseInt(report.updated_at) : 0), function(ret){
+        var transaction = opened_db.transaction("report", 'readwrite');
+        var objectStore = transaction.objectStore("report");
+        if (ret.data) {
+          for (var i = 0; i < ret.data.length; i ++) {
+            objectStore.put(ret.data[i]);
+
+            // 檢查最近天看過的內容是否有被加進去的
+	    check_recent_seen(ret.data[i]);
+          }
+        }
+      }, 'json');
+    });
+  });
+};
+
+var opened_db = null;
+// open db
+var get_newshelper_db = function(cb){
+  if (null !== opened_db) {
+    cb(opened_db);
+    return;
+  }
+
+  var request = indexedDB.open('newshelper', '7');
+  request.onsuccess = function(event){
+    opened_db = request.result;
+    cb(opened_db);
+    return;
+  };
+
+  request.onerror = function(event){
+    console.log("IndexedDB error: " + event.target.errorCode);
+  };
+
+  request.onupgradeneeded = function(event){
+    try {
+      event.currentTarget.result.deleteObjectStore('report');
+    } catch (e) {}
+    var objectStore = event.currentTarget.result.createObjectStore("report", { keyPath: "id" });
+    objectStore.createIndex("news_title", "news_title", { unique: false });
+    objectStore.createIndex("news_link", "news_link", { unique: false });
+    objectStore.createIndex("updated_at", "updated_at", { unique: false });
+
+    try {
+      event.currentTarget.result.deleteObjectStore('read_news');
+    } catch (e) {}
+    var objectStore = event.currentTarget.result.createObjectStore("read_news", { keyPath: "id", autoIncrement: true });
+    objectStore.createIndex("title", "title", { unique: false });
+    objectStore.createIndex("link", "link", { unique: true });
+    objectStore.createIndex("last_seen_at", "last_seen_at", { unique: false });
+  };
+};
+
+// get newest report in db
+var get_recent_report = function(cb){
+  get_newshelper_db(function(opened_db){
+    var transaction = opened_db.transaction('report', 'readonly');
+    var objectStore = transaction.objectStore('report');
+    var index = objectStore.index("updated_at");
+    var request = index.openCursor(null, 'prev');
+    request.onsuccess = function(){
+      if (request.result) {
+        cb(request.result.value);
+        return;
+      }
+      cb(null);
+    };
+  });
+};
+
+// log link in read_news table
+var log_browsed_link = function(link, title) {
+  if (!link) {
+    return;
+  }
+  if (seen_link[link]) {
+    return;
+  }
+  seen_link[link] = true;
+
+  get_newshelper_db(function(opened_db){
+    var transaction = opened_db.transaction("read_news", 'readwrite');
+    var objectStore = transaction.objectStore("read_news");
+    var request = objectStore.add({
+      title: title,
+      link: link,
+      last_seen_at: Math.floor((new Date()).getTime() /1000)
+    });
+
+    // link 重覆
+    request.onerror = function(){
+      var transaction = opened_db.transaction("read_news", 'readwrite');
+      var objectStore = transaction.objectStore("read_news");
+      var index = objectStore.index('link');
+      var get_request = index.get(link);
+      get_request.onsuccess = function(){
+        if (!get_request.result) {
+          console.log('link=' + link + ' is not found in IndexedDB');
+          return;
+        }
+        // update last_seen_at
+        var put_request = objectStore.put({
+          id: get_request.result.id,
+          title: title,
+          last_seen_at: Math.floor((new Date()).getTime() /1000)
+        });
+      };
+    };
+  });
+};
+
+// check recent seen news with report
+var check_recent_seen = function(report){
+  if (parseInt(report.deleted_at, 10)) {
+      return;
+  }
+  get_newshelper_db(function(opened_db){
+    var transaction = opened_db.transaction("read_news", 'readonly');
+    var objectStore = transaction.objectStore("read_news");
+    var index = objectStore.index('link');
+    var get_request = index.get(report.news_link);
+    get_request.onsuccess = function() {
+      if (!get_request.result) {
+        return;
+      };
+
+      // 如果已經被刪除了就跳過
+      add_notification(
+	'新聞小幫手提醒您',
+	'您於' + get_time_diff(get_request.result.last_seen_at) + ' 看的新聞「' + (get_request.result.title?get_request.result.title:report.news_title) + '」 被人回報有錯誤：' + report.report_title,
+        report.report_link
+      );
+    };
+  });
+};
+
+// check report by title & url
+var check_report = function(title, url, cb){
+  get_newshelper_db(function(opened_db){
+    var transaction = opened_db.transaction("report", 'readonly');
+    var objectStore = transaction.objectStore("report");
+    var index = objectStore.index('news_link');
+
+    var get_request = index.get(url);
+    get_request.onsuccess = function(){
+      // 如果有找到結果，並且沒有被刪除
+      if (get_request.result && !parseInt(get_request.result.deleted_at, 10)) {
+        return cb(get_request.result);
+      }
+      cb(false);
+    };
+  });
+};
+
+// get time diff message
+var get_time_diff = function(time){
+  var delta = Math.floor((new Date()).getTime() / 1000) - time;
+  if (delta < 60) {
+    return delta + " 秒前";
+  } else if (delta < 60 * 60) {
+    return Math.floor(delta / 60) + " 分鐘前";
+  } else if (delta < 60 * 60 * 24) {
+    return Math.floor(delta / 60 / 60) + " 小時前";
+  } else {
+    return Math.floor(delta / 60 / 60 / 24) + " 天前";
+  }
+};
 
 // show notification
 var add_notification = function(title, body, link){
--- content_script.js
+++ content_script.js
@@ -1,187 +1,21 @@
 var DEBUG_ = false;
 
-var get_newshelper_db = function(cb){
-  if (null !== opened_db) {
-    cb(opened_db);
-    return;
-  }
-
-  var request = indexedDB.open('newshelper', '6');
-  request.onsuccess = function(event){
-    opened_db = request.result;
-    cb(opened_db);
-    return;
-  };
-
-  request.onerror = function(event){
-    console.log("IndexedDB error: " + event.target.errorCode);
-  };
-
-  request.onupgradeneeded = function(event){
-    try {
-      event.currentTarget.result.deleteObjectStore('read_news');
-    } catch (e) {}
-    var objectStore = event.currentTarget.result.createObjectStore("read_news", { keyPath: "id", autoIncrement: true });
-    objectStore.createIndex("title", "title", { unique: false });
-    objectStore.createIndex("link", "link", { unique: true });
-    objectStore.createIndex("last_seen_at", "last_seen_at", { unique: false });
-
-    try {
-      event.currentTarget.result.deleteObjectStore('report');
-    } catch (e) {}
-    var objectStore = event.currentTarget.result.createObjectStore("report", { keyPath: "id" });
-    objectStore.createIndex("news_title", "news_title", { unique: false });
-    objectStore.createIndex("news_link", "news_link", { unique: false });
-    objectStore.createIndex("updated_at", "updated_at", { unique: false });
-  };
-};
-
-var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-var opened_db = null;
-
-var get_time_diff = function(time){
-  var delta = Math.floor((new Date()).getTime() / 1000) - time;
-  if (delta < 60) {
-    return delta + " 秒前";
-  } else if (delta < 60 * 60) {
-    return Math.floor(delta / 60) + " 分鐘前";
-  } else if (delta < 60 * 60 * 24) {
-    return Math.floor(delta / 60 / 60) + " 小時前";
-  } else {
-    return Math.floor(delta / 60 / 60 / 24) + " 天前";
-  }
-};
-
-
-var check_recent_seen = function(report){
-  if (parseInt(report.deleted_at, 10)) {
-      return;
-  }
-  get_newshelper_db(function(opened_db){
-    var transaction = opened_db.transaction("read_news", 'readonly');
-    var objectStore = transaction.objectStore("read_news");
-    var index = objectStore.index('link');
-    var get_request = index.get(report.news_link);
-    get_request.onsuccess = function() {
-      if (!get_request.result) {
-        return;
-      };
-
-      // 如果已經被刪除了就跳過
-      chrome.extension.sendRequest({
-        method: 'add_notification',
-        title: '新聞小幫手提醒您',
-        body: '您於' + get_time_diff(get_request.result.last_seen_at) + ' 看的新聞「' + (get_request.result.title?get_request.result.title:report.news_title) + '」 被人回報有錯誤：' + report.report_title,
-        link: report.report_link
-      }, function(response){});
-    };
-  });
-};
-
-
-var get_recent_report = function(cb){
-  get_newshelper_db(function(opened_db){
-    var transaction = opened_db.transaction('report', 'readonly');
-    var objectStore = transaction.objectStore('report');
-    var index = objectStore.index("updated_at");
-    var request = index.openCursor(null, 'prev');
-    request.onsuccess = function(){
-      if (request.result) {
-        cb(request.result.value);
-        return;
-      }
-      cb(null);
-    };
-  });
-};
-
-
-// 跟遠端 API server 同步回報資料
+// 叫 background 開始跟 api server 同步資料
 var sync_report_data = function(){
-  get_newshelper_db(function(opened_db){
-    get_recent_report(function(report){
-      $.get('http://newshelper.g0v.tw/index/data?time=' + (report ? parseInt(report.updated_at) : 0), function(ret){
-        var transaction = opened_db.transaction("report", 'readwrite');
-        var objectStore = transaction.objectStore("report");
-        if (ret.data) {
-          for (var i = 0; i < ret.data.length; i ++) {
-            objectStore.put(ret.data[i]);
-
-            // 檢查最近天看過的內容是否有被加進去的
-            check_recent_seen(ret.data[i]);
-          }
-        }
-
-        // 每 5 分鐘去檢查一次是否有更新
-        setTimeout(sync_report_data, 300000);
-      }, 'json');
-    });
-  });
+    chrome.extension.sendRequest({method: 'start_sync_db'}, function(response){});
 };
-
-
-var seen_link = {};
-var log_browsed_link = function(link, title) {
-  if (!link) {
-    return;
-  }
-  if (seen_link[link]) {
-    return;
-  }
-  seen_link[link] = true;
-
-  get_newshelper_db(function(opened_db){
-    var transaction = opened_db.transaction("read_news", 'readwrite');
-    var objectStore = transaction.objectStore("read_news");
-    var request = objectStore.add({
-      title: title,
-      link: link,
-      last_seen_at: Math.floor((new Date()).getTime() /1000)
-    });
-
-    // link 重覆
-    request.onerror = function(){
-      var transaction = opened_db.transaction("read_news", 'readwrite');
-      var objectStore = transaction.objectStore("read_news");
-      var index = objectStore.index('link');
-      var get_request = index.get(link);
-      get_request.onsuccess = function(){
-        if (!get_request.result) {
-          console.log('link=' + link + ' is not found in IndexedDB');
-          return;
-        }
-        // update last_seen_at
-        var put_request = objectStore.put({
-          id: get_request.result.id,
-          title: title,
-          last_seen_at: Math.floor((new Date()).getTime() /1000)
-        });
-      };
-    };
-  });
-};
-
 
 // 從 db 中判斷 title, url 是否是錯誤新聞，是的話執行 cb 並傳入資訊
 var check_report = function(title, url, cb){
   if (!url) {
     return;
   }
-  get_newshelper_db(function(opened_db){
-    var transaction = opened_db.transaction("report", 'readonly');
-    var objectStore = transaction.objectStore("report");
-    var index = objectStore.index('news_link');
-
-    var get_request = index.get(url);
-    get_request.onsuccess = function(){
-      // 如果有找到結果，並且沒有被刪除
-      if (get_request.result && !parseInt(get_request.result.deleted_at, 10)) {
-        cb(get_request.result);
+  chrome.extension.sendRequest({method: 'check_report', title: title, url: url}, function(ret){
+      if (ret !== false) {
+	  cb(ret);
       }
-    };
   });
-};
-
+}
 
 var buildWarningMessage = function(options){
   return '<div class="newshelper-warning-facebook">' +
@@ -277,7 +111,7 @@ var censorFacebook = function(baseNode) {
       if (DEBUG_ && !addedAction) console.log('fail to insert actionbar');
 
       /* log the link first */
-      log_browsed_link(linkHref, titleText);
+      chrome.extension.sendRequest({method: 'log_browsed_link', title: titleText, link: linkHref}, function(response){});
 
       check_report(titleText, linkHref, function(report){
         containerNode.addClass(className);
