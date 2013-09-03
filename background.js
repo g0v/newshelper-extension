58039c08ba7f3987d1b0c73e3e60c44c52fe4b84
Ronny Wang <ronnywang@gmail.com>
2013-09-03 18:18:38 +0800

    move code to add_notification function

--- background.js
+++ background.js
@@ -5,15 +5,7 @@ function onRequest(request, sender, sendResponse) {
     }
 
     if (request.method == 'add_notification') {
-        var notification = window.webkitNotifications.createNotification(
-              'newshelper48x48.png',
-              '' + request.title,
-              '' + request.body
-        );
-	notification.onclick = function(){
-	    window.open(request.link);
-	};
-        notification.show();
+	add_notification(request.title, request.body, request.link);
     }
 
     // Return nothing to let the connection be cleaned up.
@@ -23,3 +15,16 @@ function onRequest(request, sender, sendResponse) {
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
 
+
+// show notification
+var add_notification = function(title, body, link){
+  var notification = window.webkitNotifications.createNotification(
+    'newshelper48x48.png',
+    '' + title,
+    '' + body
+  );
+  notification.onclick = function(){
+    window.open(link);
+  };
+  notification.show();
+}
